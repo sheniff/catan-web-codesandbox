fd524aaeba941c05eb8b05bdfacb044d3a869f0e
Sergio Almécija <sheniff@gmail.com>
2022-01-18 02:04:51 -0800

    responsive board

--- src/view/CatanBoard.tsx
+++ src/view/CatanBoard.tsx
@@ -3,27 +3,50 @@ import { HexGrid, Layout } from 'react-hexgrid';
 import { Board } from '../engine/board';
 import { Tile, TileType } from '../engine/tile';
 import { CatanTile } from './CatanTile';
+import styled from 'styled-components';
 
 interface Props {
   board: Board;
 }
 
+const StyledWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 100%;
+  padding-bottom: 100%;
+  vertical-align: middle;
+  overflow: hidden;
+`;
+
+const StyledSvg = styled.svg`
+  display: inline-block;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
 export const CatanBoard: React.FC<Props> = ({ board }) => {
   const hexagons = board.getHexes();
 
   return (
-    <HexGrid width={450} height={450} viewBox="-43 -50 100 100">
-      <Layout size={{ x: 7, y: 7 }} spacing={1.02} flat={false}>
-        {hexagons
-          // Get tile for each hex
-          .map((hex) => ({ hex, tile: board.getTile(hex) as Tile }))
-          // Ignore offset ring
-          .filter(({ tile }) => tile.getTileType() === TileType.TILE)
-          // Render
-          .map(({ hex, tile }, i: number) => (
-            <CatanTile key={i} hex={hex} tile={tile} />
-          ))}
-      </Layout>
-    </HexGrid>
+    <StyledWrapper>
+      <StyledSvg
+        viewBox="-35 -30 70 60"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <Layout size={{ x: 7, y: 7 }} spacing={1.02} flat={false}>
+          {hexagons
+            // Get tile for each hex
+            .map((hex) => ({ hex, tile: board.getTile(hex) as Tile }))
+            // Ignore offset ring
+            .filter(({ tile }) => tile.getTileType() === TileType.TILE)
+            // Render
+            .map(({ hex, tile }, i: number) => (
+              <CatanTile key={i} hex={hex} tile={tile} />
+            ))}
+        </Layout>
+      </StyledSvg>
+    </StyledWrapper>
   );
 };
